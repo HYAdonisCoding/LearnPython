f3a3557791844db45ddb1b3e8c2e31d99ef4154b
曹弘扬 <296786475@qq.com>
2020-04-12 14:55:33 +0800

    dict

--- Python3Base.py
+++ Python3Base.py
@@ -4,7 +4,7 @@
 '''
 @Author: Adam
 @Date: 2020-04-09 06:36:03
-@LastEditTime: 2020-04-12 14:14:08
+@LastEditTime: 2020-04-12 14:54:45
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /undefined/Users/adonis/Developer/MyGithubCode/LearnPython/Python3Base.py
@@ -167,6 +167,7 @@ else:
 
 # 循环
 # ---------------------------------------- start ----------------------------------------
+'''
 names = ['Michael', 'Bob', 'Tracy']
 for name in names:
     print(name)
@@ -186,10 +187,10 @@ while n > 0:
     sum = sum + n
     n = n - 2
 print(sum)
-'''
-练习
-请利用循环依次对list中的每个名字打印出Hello, xxx!：
-'''
+
+# 练习
+# 请利用循环依次对list中的每个名字打印出Hello, xxx!：
+
 L = ['Bart', 'Lisa', 'Adam']
 for name in L:
     print('Hello, '+name+'!')
@@ -209,6 +210,46 @@ while n < 10:
     print(n)
     
     
-    
+'''
+# ---------------------------------------- end ----------------------------------------
+
+# dict
+# ---------------------------------------- start ----------------------------------------
+print('-----------dict-----------')
+names = ['Michael', 'Bob', 'Tracy']
+scores = [95, 75, 85]
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print(d['Michael'])
+d['Adam'] = 78
+print(d['Adam'])
+# 要避免key不存在的错误，有两种办法，一是通过in判断key是否存在：
+print('Alice' in d)
+# 二是通过dict提供的get()方法，如果key不存在，可以返回None，或者自己指定的value：
+print(d.get('Alice'))
+print(d.get('Alice', -1))
+print(d.get('Adam'))
+print(d.get('Adam', -1))
+# 要删除一个key，用pop(key)方法，对应的value也会从dict中删除：
+print(d.pop('Bob'))
+print(d)
+# 请务必注意，dict内部存放的顺序和key放入的顺序是没有关系的。
+# 和list比较，dict有以下几个特点：
+# 查找和插入的速度极快，不会随着key的增加而变慢；
+# 需要占用大量的内存，内存浪费多。
+# 而list相反：
+# 查找和插入的时间随着元素的增加而增加；
+# 占用空间小，浪费内存很少。
+# 所以，dict是用空间来换取时间的一种方法。
+# dict可以用在需要高速查找的很多地方，在Python代码中几乎无处不在，正确使用dict非常重要，需要牢记的第一条就是dict的key必须是不可变对象。
+# 这是因为dict根据key来计算value的存储位置，如果每次计算相同的key得出的结果不同，那dict内部就完全混乱了。这个通过key计算位置的算法称为哈希算法（Hash）。
+# 要保证hash的正确性，作为key的对象就不能变。在Python中，字符串、整数等都是不可变的，因此，可以放心地作为key。而list是可变的，就不能作为key：
+key = [1, 2, 3]
+# d[key] = 'a list' TypeError: unhashable type: 'list'
+
+# ---------------------------------------- end ----------------------------------------
+
+# set
+# ---------------------------------------- start ----------------------------------------
+# print('-----------set-----------')
 # ---------------------------------------- end ----------------------------------------
 print('end')
