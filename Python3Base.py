bbee94aa7e4061583205768d1f0d568ab1a7b2b8
曹弘扬 <296786475@qq.com>
2020-04-12 17:08:33 +0800

    function

--- Python3Base.py
+++ Python3Base.py
@@ -4,7 +4,7 @@
 '''
 @Author: Adam
 @Date: 2020-04-09 06:36:03
-@LastEditTime: 2020-04-12 15:12:27
+@LastEditTime: 2020-04-12 17:08:12
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /undefined/Users/adonis/Developer/MyGithubCode/LearnPython/Python3Base.py
@@ -251,6 +251,7 @@ key = [1, 2, 3]
 
 # set
 # ---------------------------------------- start ----------------------------------------
+'''
 print('-----------set-----------')
 # set和dict类似，也是一组key的集合，但不存储value。由于key不能重复，所以，在set中，没有重复的key。
 # 要创建一个set，需要提供一个list作为输入集合：
@@ -289,7 +290,299 @@ d[k1] = "k1"
 print(d)
 # d[k2] = 'k2'
 # print(d)
+'''
+# ---------------------------------------- end ----------------------------------------
 
+# 函数
+# ---------------------------------------- start ----------------------------------------
+
+print('-----------函数-----------')
+# 绝对值
+print(abs(100))
+print(abs(-20))
+print(abs(12.34))
+# print(abs(1,2))
+# print(abs('a'))
+# 最值
+print(max(1,2))
+print(max(1,2, 5, 8 ,-10))
+print(min(1,2, 5, 8 ,-10))
+# 数据类型转换
+print(int('1234'))
+print(int(12.34))
+print(float('12.34'))
+print(str(1.23))
+print(str(100))
+print(bool(1))
+print(bool(0))
+# 函数名其实就是指向一个函数对象的引用，完全可以把函数名赋给一个变量，相当于给这个函数起了一个“别名”：
+a = abs
+print(a(-100))
+
+# 练习
+# 请利用Python内置的hex()函数把一个整数转换成十六进制表示的字符串：
+a = 255
+a = 1000
+b = hex(a)
+c = str(b)
+print(b)
+print(c)
+
+# 定义函数
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(my_abs(-99))
+# 如果你已经把my_abs()的函数定义保存为abstest.py文件了，那么，可以在该文件的当前目录下启动Python解释器，用from abstest import my_abs来导入my_abs()函数，注意abstest是文件名（不含.py扩展名）：
+
+# 空函数
+def nop():
+    pass
+# my_abs(1,2)
+# my_abs('A')
+
+# 返回多个值
+import math
+
+def move(x, y, step, angle = 0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+
+x, y = move(100, 100, 60, math.pi / 6)
+print(x)
+print(y)
+print(x, y)
+r = move(100, 100, 60, math.pi / 6)
+print(r)
+
+# 小结
+
+# 定义函数时，需要确定函数名和参数个数；
+
+# 如果有必要，可以先对参数的数据类型做检查；
+
+# 函数体内部可以用return随时返回函数结果；
+
+# 函数执行完毕也没有return语句时，自动return None。
+
+# 函数可以同时返回多个值，但其实就是一个tuple。
+
+# 练习
+# 请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程 ax**2+bx+c=0 的两个解。
+# 提示：
+# 一元二次方程的求根公式为：
+# 计算平方根可以调用math.sqrt()函数：
+def quadratic(a, b, c):
+    z1 = -b + math.sqrt(b**2-4*a*c)
+    z2 = -b - math.sqrt(b**2-4*a*c)
+    m = 2*a
+    return z1/m, z2/m
+
+print(quadratic(2, 3, 1))
+if quadratic(2, 3, 1) != (-0.5, -1.0):
+    print('测试失败')
+elif quadratic(1, 3, -4) != (1.0, -4.0):
+    print('测试失败')
+else:
+    print('测试成功')
+
+# 函数的参数
+def power(x):
+    return x * x
+print(power(5))
+print(power(15))
+
+def power(x, n=2):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+    
+print(power(5, 2))
+print(power(5, 3))
+
+print(power(5))
+# 默认参数很有用，但使用不当，也会掉坑里。默认参数有个最大的坑，演示如下：
+
+# 先定义一个函数，传入一个list，添加一个END再返回：
+
+def add_end(L=[]):
+    L.append('END')
+    return L
+a = add_end([1, 2, 3])
+print(a)
+a = add_end()
+print(a)
+a = add_end()
+print(a)
+a = add_end()
+print(a)
+# 原因解释如下：
+# Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+a = add_end()
+print(a)
+a = add_end()
+print(a)
+a = add_end()
+print(a)
+
+# 可变参数
+# 在Python函数中，还可以定义可变参数。顾名思义，可变参数就是传入的参数个数是可变的，可以是1个、2个到任意个，还可以是0个。
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+print(calc([1, 2, 3]))
+print(calc((1, 3, 5, 7)))
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+print(calc(1, 2, 3))
+print(calc(1, 3, 5, 7))
+print(calc())
+n = [1, 2, 3]
+print(calc(*n))
+# *nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见。
+
+# 关键字参数
+# 可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict。请看示例：
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+person('Michael', 38)
+person('Bob', 35, city='Beijing')
+person('Adam',25, gender='Super', job='Engineer')
+extra = {'city': 'Beijing', 'job': 'Engineer', 'addr': '中南海'}
+person('Jack', 24, city=extra['city'], job=extra['job'])
+person('Jack', 24, **extra)
+# **extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
+
+# 命名关键字参数
+# 对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于到底传入了哪些，就需要在函数内部通过kw检查。
+def person(name, age, **kw):
+    if 'city' in kw:
+        pass
+    if 'job' in kw:
+        pass
+    print('name:', name, 'age:', age, 'other:', kw)
+
+# person('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
+person('Jack', 24, **extra)
+
+def person(name, age, *, city, job):
+    print(name, age, city, job)
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, **extra)
+
+# 如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了：
+def person(name, age, *args, city, job):
+    print(name, age, args, city, job)
+# 命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错：
+# person('Jack', 24, 'Beijing', 'Engineer')
+person('Jack', 24, city='Engineer', job='Engineer')
+person('Jack', 24, job='Engineer',city='Engineer')
+
+def person(name, age, *, city='Beijing', job):
+    print(name, age, city, job)
+
+person('Jack', 24, job='Engineer')
+
+# 参数组合
+# 在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
+def f1(a, b, c=0, *args, **kw):
+    print('a=', a, 'b=', b, 'c=', c, 'args=', args,'kw=', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+     print('a=', a, 'b=', b, 'c=', c, 'd=', d,'kw=', kw)
+f1(1, 2)
+f1(1, 2, c=3)
+f1(1, 2, 3, 'a', 'b')
+f1(1, 2, 3, 'a', 'b', x=99)
+f2(1, 2, d=99, ext=None)
+args = (1, 2, 3, 4)
+kw = {'d': 99, 'x': '#'}
+f1(*args, **kw)
+args = (1, 2, 3)
+kw = {'d': 00, 'x': '#'}
+f2(*args, **kw)
+# 虽然可以组合多达5种参数，但不要同时使用太多的组合，否则函数接口的可理解性很差。
+
+# 练习
+# 以下函数允许计算两个数的乘积，请稍加改造，变成可接收一个或多个数并计算乘积：
+def product(x, y):
+    sum = x * y
+    print(sum)
+    return sum
+    
+product(5,10)
+
+def product(*x):
+    if len(x) <= 0:
+        raise TypeError('params can\'t be none')
+    sum = 1
+    for num in x:
+        sum = sum * num
+    print(sum)
+    return sum
+
+product(5)
+product(5, 6)
+product(5, 6, 7)
+product(5, 6, 7, 9)
+# 测试
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else: 
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
+product()
+# 小结
+
+# Python的函数具有非常灵活的参数形态，既可以实现简单的调用，又可以传入非常复杂的参数。
+
+# 默认参数一定要用不可变对象，如果是可变对象，程序运行时会有逻辑错误！
+
+# 要注意定义可变参数和关键字参数的语法：
+
+# *args是可变参数，args接收的是一个tuple；
+
+# **kw是关键字参数，kw接收的是一个dict。
+
+# 以及调用函数时如何传入可变参数和关键字参数的语法：
+
+# 可变参数既可以直接传入：func(1, 2, 3)，又可以先组装list或tuple，再通过*args传入：func(*(1, 2, 3))；
+
+# 关键字参数既可以直接传入：func(a=1, b=2)，又可以先组装dict，再通过**kw传入：func(**{'a': 1, 'b': 2})。
+
+# 使用*args和**kw是Python的习惯写法，当然也可以用其他参数名，但最好使用习惯用法。
+
+# 命名的关键字参数是为了限制调用者可以传入的参数名，同时可以提供默认值。
+
+# 定义命名的关键字参数在没有可变参数的情况下不要忘了写分隔符*，否则定义的将是位置参数。
 
 # ---------------------------------------- end ----------------------------------------
+
+
 print('end')
