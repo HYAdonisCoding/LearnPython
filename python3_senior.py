3038ca5bc2026341ebb0a973cbb6b695fd48efa7
曹弘扬 <296786475@qq.com>
2020-04-14 21:55:54 +0800

    高级特征 列表生成式

--- python3_senior.py
+++ python3_senior.py
@@ -4,7 +4,7 @@
 '''
 @Author: Adam
 @Date: 2020-04-13 16:48:59
-@LastEditTime: 2020-04-14 06:55:28
+@LastEditTime: 2020-04-14 21:54:46
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /LearnPython/python3_senior.py
@@ -117,6 +117,7 @@ else:
 
 # 迭代
 # ---------------------------------------- start ----------------------------------------
+'''
 print('-----------迭代-----------')
 # 如果给定一个list或tuple，我们可以通过for循环来遍历这个list或tuple，这种遍历我们称为迭代（Iteration）。
 # 在Python中，迭代是通过for ... in来完成的，而很多语言比如C语言，迭代list是通过下标完成的，比如Java代码：
@@ -178,5 +179,85 @@ else:
 print(findMinAndMax('abc'))
 
 print(findMinAndMax(['a','b',1]))
+# ---------------------------------------- end ----------------------------------------
+
+'''
+# 列表生成式
+# ---------------------------------------- start ----------------------------------------
+print('-----------列表生成式-----------')
+# 列表生成式即List Comprehensions，是Python内置的非常简单却强大的可以用来创建list的生成式。
+L = list(range(1, 11))
+print(L)
+# 但如果要生成[1x1, 2x2, 3x3, ..., 10x10]怎么做？方法一是循环：
+L = []
+for x in range(1, 11):
+    L.append(x ** 2)
+print(L)
+
+# 但是循环太繁琐，而列表生成式则可以用一行语句代替循环生成上面的list：
+L = [x ** 2 for x in range(1, 11)]
+print(L)
+
+# 写列表生成式时，把要生成的元素x * x放到前面，后面跟for循环，就可以把list创建出来，十分有用，多写几次，很快就可以熟悉这种语法。
+# for循环后面还可以加上if判断，这样我们就可以筛选出仅偶数的平方：
+L = [x ** 2 for x in range(1, 11) if x % 2 == 0]
+print(L)
+# 还可以使用两层循环，可以生成全排列：
+L = [m + n for m in 'ABC' for n in 'XYZ']
+print(L)
+# 三层和三层以上的循环就很少用到了。
+
+# 运用列表生成式，可以写出非常简洁的代码。例如，列出当前目录下的所有文件和目录名，可以通过一行代码实现
+import os 
+L = [d for d in os.listdir('.')]
+print(L)
+
+# for循环其实可以同时使用两个甚至多个变量，比如dict的items()可以同时迭代key和value：
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+for k, v in d.items():
+    print(k, '=', v)
+
+# 因此，列表生成式也可以使用两个变量来生成list：
+L = [k + '=' + v for k, v in d.items()]
+print(L)
+
+# 最后把一个list中所有的字符串变成小写：
+L = ['HELLO', 'WORLD', 'IBM', 'Apple']
+L1 = [s.lower() for s in L]
+print(L1)
+
+# if ... else
+
+# 使用列表生成式的时候，有些童鞋经常搞不清楚if...else的用法。
+
+# 例如，以下代码正常输出偶数：
+L = [x for x in range(1, 11) if x % 2 == 0]
+print(L)
+# 但是，我们不能在最后的if加上else：
+# L = [x for x in range(1, 11) if x % 2 == 0 else 0]
+# 这是因为跟在for后面的if是一个筛选条件，不能带else，否则如何筛选？
+
+# 另一些童鞋发现把if写在for前面必须加else，否则报错：
+# L = [x if x % 2 == 0 for x in range(1, 11)]
+# 这是因为for前面的部分是一个表达式，它必须根据x计算出一个结果。因此，考察表达式：x if x % 2 == 0，它无法根据x计算出结果，因为缺少else，必须加上else：
+L = [x if x % 2 == 0 else -x for x in range(1, 11)]
+print(L)
+
+# 上述for前面的表达式x if x % 2 == 0 else -x才能根据x计算出确定的结果。
+
+# 可见，在一个列表生成式中，for前面的if ... else是表达式，而for后面的if是过滤条件，不能带else。
+
+# 练习
+# 如果list中既包含字符串，又包含整数，由于非字符串类型没有lower()方法，所以列表生成式会报错：
+L = ['Hello', 'World', 18, 'Apple', None]
+# L1 = [s.lower() for s in L]
+print(L)
+L2 = [s.lower() for s in L if isinstance(s, str)]
+# 测试:
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
 # ---------------------------------------- end ----------------------------------------
 print('-----------End-----------')
