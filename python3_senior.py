2e2ab111e3c64eb9dcf06a13555ea4808173912f
曹弘扬 <296786475@qq.com>
2020-04-17 16:08:30 +0800

    高级特征 生成器 杨辉三角待思考

--- python3_senior.py
+++ python3_senior.py
@@ -4,7 +4,7 @@
 '''
 @Author: Adam
 @Date: 2020-04-13 16:48:59
-@LastEditTime: 2020-04-14 21:54:46
+@LastEditTime: 2020-04-17 16:07:33
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /LearnPython/python3_senior.py
@@ -184,6 +184,7 @@ print(findMinAndMax(['a','b',1]))
 '''
 # 列表生成式
 # ---------------------------------------- start ----------------------------------------
+'''
 print('-----------列表生成式-----------')
 # 列表生成式即List Comprehensions，是Python内置的非常简单却强大的可以用来创建list的生成式。
 L = list(range(1, 11))
@@ -259,5 +260,171 @@ if L2 == ['hello', 'world', 'apple']:
     print('测试通过!')
 else:
     print('测试失败!')
+'''
+# ---------------------------------------- end ----------------------------------------
+
+# 生成器
+# ---------------------------------------- start ----------------------------------------
+print('-----------生成器-----------')
+# 通过列表生成式，我们可以直接创建一个列表。但是，受到内存限制，列表容量肯定是有限的。而且，创建一个包含100万个元素的列表，不仅占用很大的存储空间，如果我们仅仅需要访问前面几个元素，那后面绝大多数元素占用的空间都白白浪费了。
+
+# 所以，如果列表元素可以按照某种算法推算出来，那我们是否可以在循环的过程中不断推算出后续的元素呢？这样就不必创建完整的list，从而节省大量的空间。在Python中，这种一边循环一边计算的机制，称为生成器：generator。
+
+# 要创建一个generator，有很多种方法。第一种方法很简单，只要把一个列表生成式的[]改成()，就创建了一个generator：
+L = [x ** 2 for x in range(10)]
+print(L)
+g = (x ** 2 for x in range(10))
+# print(g)
+# 创建L和g的区别仅在于最外层的[]和()，L是一个list，而g是一个generator。
+
+# 我们可以直接打印出list的每一个元素，但我们怎么打印出generator的每一个元素呢？
+
+# 如果要一个一个打印出来，可以通过next()函数获得generator的下一个返回值：
+# print(next(g), next(g), next(g), next(g), next(g), next(g), next(g), next(g), next(g), next(g))
+# print(next(g), next(g), next(g), next(g), next(g), next(g), next(g), next(g), next(g), next(g))
+# 我们讲过，generator保存的是算法，每次调用next(g)，就计算出g的下一个元素的值，直到计算到最后一个元素，没有更多的元素时，抛出StopIteration的错误。
+
+# 当然，上面这种不断调用next(g)实在是太变态了，正确的方法是使用for循环，因为generator也是可迭代对象：
+for n in g:
+    print(n)
+
+# 所以，我们创建了一个generator后，基本上永远不会调用next()，而是通过for循环来迭代它，并且不需要关心StopIteration的错误。
+
+# generator非常强大。如果推算的算法比较复杂，用类似列表生成式的for循环无法实现的时候，还可以用函数来实现。
+
+# 比如，著名的斐波拉契数列（Fibonacci），除第一个和第二个数外，任意一个数都可由前两个数相加得到：
+
+# 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+# 斐波拉契数列用列表生成式写不出来，但是，用函数把它打印出来却很容易：
+def fib(max):
+    n, a, b = 0, 0 , 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+print(fib(6))
+print(fib(9))
+# 仔细观察，可以看出，fib函数实际上是定义了斐波拉契数列的推算规则，可以从第一个元素开始，推算出后续任意的元素，这种逻辑其实非常类似generator。
+
+# 也就是说，上面的函数和generator仅一步之遥。要把fib函数变成generator，只需要把print(b)改为yield b就可以了：
+def fib(max):
+    n, a , b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'DONE'
+
+# 这里，最难理解的就是generator和函数的执行流程不一样。函数是顺序执行，遇到return语句或者最后一行函数语句就返回。而变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+
+# 举个简单的例子，定义一个generator，依次返回数字1，3，5：
+def odd():
+    print('step1')
+    yield 1
+    print('step2')
+    yield 2
+    print('step3')
+    yield 3
+o = odd()
+next(o)
+next(o)
+next(o)
+# next(o)
+# 可以看到，odd不是普通函数，而是generator，在执行过程中，遇到yield就中断，下次又继续执行。执行3次yield后，已经没有yield可以执行了，所以，第4次调用next(o)就报错。
+
+# 回到fib的例子，我们在循环过程中不断调用yield，就会不断中断。当然要给循环设置一个条件来退出循环，不然就会产生一个无限数列出来。
+
+# 同样的，把函数改成generator后，我们基本上从来不会用next()来获取下一个返回值，而是直接使用for循环来迭代：
+
+for n in fib(9):
+    print(n)
+
+# 但是用for循环调用generator时，发现拿不到generator的return语句的返回值。如果想要拿到返回值，必须捕获StopIteration错误，返回值包含在StopIteration的value中：
+g = fib(9)
+while True:
+    try:
+        x = next(g)
+        print('g', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+# 练习
+
+# 杨辉三角定义如下：
+
+#           1
+#          / \
+#         1   1
+#        / \ / \
+#       1   2   1
+#      / \ / \ / \
+#     1   3   3   1
+#    / \ / \ / \ / \
+#   1   4   6   4   1
+#  / \ / \ / \ / \ / \
+# 1   5   10  10  5   1
+# 把每一行看做一个list，试写一个generator，不断输出下一行的list：
+# 解题思路
+
+## 杨辉三角形
+
+# 假设行号以1开始，每行的元素数即为行号；
+# 每行的头尾都是1；
+# 每行中间的数都是正上方的数及正上方左边的数的和。
+## 题干
+# 使用 results ，在 for 循环中，将 triangles() 生成器的元素逐行读出；
+# n 作为计数器，当 n 累加到10后，停止读出生成器的元素；
+# 使用 for 循环将 results （list）打印出来；
+# 与正确答案做比对
+def triangles():
+    m = [1]
+    while True:
+        yield m
+        m = [1] + [m[x] + m[x+1] for x in range(len(m)-1)] + [1]
+
+# 期待输出:
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+n = 0
+results = []
+for t in triangles():
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+
+for t in results:
+    print(t)
+
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+
+
+
 # ---------------------------------------- end ----------------------------------------
 print('-----------End-----------')
