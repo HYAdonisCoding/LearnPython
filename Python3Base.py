82fde07bc966773a9d2ac3a0755dd1eb57b2c3d2
Adam <296786475@qq.com>
2020-06-16 17:01:10 +0800

    GUI program

--- Python3Base.py
+++ Python3Base.py
@@ -588,7 +588,7 @@ else:
 # 在函数内部，可以调用其他函数。如果一个函数在内部调用自身本身，这个函数就是递归函数。
 def fact(n):
     if n == 1:
-        return 1;
+        return 1
     return n * fact(n-1)
 
 print(fact(1))
---
+++ python3_gui.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello , world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+
+app = Application()
+
+# 设置窗口标题
+app.master.title('World Window')
+# 主消息循环
+app.mainloop()
